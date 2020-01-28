84c4cd93485c1e3b1303bb2efd7f2d4396548a7c
Leo11177 <58432060+Leo11177@users.noreply.github.com>
2020-01-28 10:13:16 +0100

    commit 2

--- src/fr/leo/servlets/traitementFormulaire.java
+++ src/fr/leo/servlets/traitementFormulaire.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class traitementFormulaire extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	//cvgcxb
     /**
      * @see HttpServlet#HttpServlet()
      */
